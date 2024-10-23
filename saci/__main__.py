4657cbf0e4c9fc18649b10a88f4767c4d383ba39
Jessie Grosen <jmgrosen@gmail.com>
2024-10-23 17:48:30 -0400

    expose webui from main entry point

--- saci/__main__.py
+++ saci/__main__.py
@@ -3,15 +3,21 @@ import argparse
 import saci
 
 def main():
-    from saci.orchestrator import main as orchestrator_main
-
     parser = argparse.ArgumentParser()
     parser.add_argument("-r", "--run-component", type=str, help="Specify the component to run")
     parser.add_argument("-v", "--version", action="version", version=f"{saci.__version__}")
     args = parser.parse_args()
 
     if args.run_component == "orchestrator":
+        from saci.orchestrator import main as orchestrator_main
         orchestrator_main()
+    if args.run_component == "web":
+        try:
+            import flask
+        except ImportError:
+            raise RuntimeError("Please install flask before running the web component")
+        from saci.webui.web import app
+        app.run()
     else:
         raise RuntimeError("Unknown component to run")
 
