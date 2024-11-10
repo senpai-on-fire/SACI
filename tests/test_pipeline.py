c3e55d00e40cac6aa8433dfba0d819bdbd377b2c
Jessie Grosen <jmgrosen@gmail.com>
2024-11-09 20:57:51 -0500

    test identifying the rover CPVs

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -2,13 +2,21 @@ import sys
 import subprocess
 
 import unittest
+from typing import Optional
 
 import saci
+from saci.modeling import CPV, ComponentBase
+from saci.modeling.device.compass import CompassSensorHigh
+from saci.modeling.device.motor.steering import SteeringHigh
 from saci.modeling.state import GlobalState
-from saci.modeling.device import MultiCopterMotor
+from saci.modeling.device import MultiCopterMotor, TelemetryHigh, MotorHigh
 from saci.orchestrator import process, identify
 
 from saci_db.cpvs import MavlinkCPV
+from saci_db.cpvs.cpv06_roll_over import RollOverCPV
+from saci_db.cpvs.cpv07_compass_interference import CompassInterferenceCPV
+from saci_db.cpvs.cpv08_web_stop import WebStopCPV
+from saci_db.devices.ngcrover import NGCRover
 from saci_db.vulns import MavlinkCPSV
 from saci_db.devices.px4_quadcopter_device import PX4Quadcopter, GCSTelemetry
 
@@ -46,6 +54,27 @@ class TestPipeline(unittest.TestCase):
         path = cpv_paths[0].path
         self.assertIsInstance(path[0], GCSTelemetry)
         self.assertIsInstance(path[-1], MultiCopterMotor)
+
+    def test_identifier_rover(self):
+        cps = NGCRover()
+        initial_state = GlobalState(cps.components)
+        # this rover sure is vulnerable
+        cpv_hypotheses: list[tuple[CPV, Optional[tuple[type[ComponentBase], type[ComponentBase]]]]] = [
+            (MavlinkCPV(), None),
+            (RollOverCPV(), (TelemetryHigh, SteeringHigh)),
+            (CompassInterferenceCPV(), (CompassSensorHigh, SteeringHigh)),
+            (WebStopCPV(), (TelemetryHigh, MotorHigh)),
+        ]
+        for cpv, endpoints in cpv_hypotheses:
+            _, cpv_paths = identify(cps, initial_state, cpv_model=cpv)
+            if endpoints is not None:
+                start, end = endpoints
+                self.assertGreater(len(cpv_paths), 0)
+                for cpv_path in cpv_paths:
+                    self.assertIsInstance(cpv_path.path[0], start)
+                    self.assertIsInstance(cpv_path.path[-1], end)
+            else:
+                self.assertIsNone(cpv_paths)
 
 
 if __name__ == "__main__":
