b75b1e9e184c933ae9cc3233d3008f963f260572
Tiffany Bao <tiffany.yzh@gmail.com>
2024-11-14 14:36:39 -0500

    add atoms

---
+++ saci/atoms.py
@@ -0,0 +1,11 @@
+Atoms = [
+  {
+    "Kinetic Effect": "Roll over",
+    "Independent Variables": ["Tilt Angle", "Friction"],
+    "Required Components": ["Telemetry", "Controller", "Steering"],
+    "Dependent Variables": ["Velocity"],
+    "Properties": {
+      "Velocity": "Sudden drop to 0"
+    }
+  }
+]
