6a0082414390dacd3f33c6a257dd046b462b9202
HalimaBouzidi <fh_bouzidi@esi.dz>
2024-12-03 14:44:37 -0800

    Updated Attack

--- saci/modeling/attack/base_attack_vector.py
+++ saci/modeling/attack/base_attack_vector.py
@@ -1,23 +1,17 @@
 
-from typing import List, Type, Optional, Dict, Tuple
-from saci.modeling.device.component import CyberComponentBase
 from .base_attack_signal import BaseAttackSignal
 
 class BaseAttackVector:
     def __init__(self, 
                  name: str,
-                 src: str,
                  signal:BaseAttackSignal, 
-                 dst:CyberComponentBase, 
                  required_access_level: str,
                  configuration: tuple[str, ...] = (),
                  options: tuple[str, ...] = (),
     ):
         
         self.name = name
-        self.src = src
         self.signal = signal
-        self.dst = dst
         self.required_access_level = required_access_level
         self.configuration = configuration
         self.options = options
