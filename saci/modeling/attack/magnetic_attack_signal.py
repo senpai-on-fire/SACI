b3df11d595ed200bcd779bf8a65b3643fc1521f4
HalimaBouzidi <fh_bouzidi@esi.dz>
2024-12-03 11:30:52 -0800

    Updated CPV07

--- saci/modeling/attack/magnetic_attack_signal.py
+++ saci/modeling/attack/magnetic_attack_signal.py
@@ -3,4 +3,4 @@ from .base_attack_signal import BaseAttackSignal
 
 class MagneticAttackSignal(BaseAttackSignal):
     def __init__(self, src=None, dst=None, modality="magnetic"):
-        super().__init__(src=src, dst=dst, data=modality)
+        super().__init__(src=src, dst=dst, modality=modality)
