721a9cb32c558919f80967285283083e9dcbb1e5
HalimaBouzidi <fh_bouzidi@esi.dz>
2024-12-02 21:22:52 -0800

    Controller and Telemetry Tweaks

--- saci/modeling/device/controller.py
+++ saci/modeling/device/controller.py
@@ -5,7 +5,7 @@ class ControllerHigh(CyberComponentHigh):
     __state_slots__ = CyberComponentHigh.__state_slots__ + ("in_failsafe_mode",)
     __slots__ = CyberComponentHigh.__slots__ + ("in_failsafe_mode",)
 
-    def __init__(self, in_failsafe_mode=False, **kwargs):
+    def __init__(self, in_failsafe_mode=False, integrity_check=False, **kwargs):
         """
 
 
@@ -14,6 +14,7 @@ class ControllerHigh(CyberComponentHigh):
         """
         super().__init__(**kwargs)
         self.in_failsafe_mode = in_failsafe_mode
+        self.integrity_check = integrity_check
 
 
 class Controller(CyberComponentBase):
