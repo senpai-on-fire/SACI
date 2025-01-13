2ac2614e7890887a9e88d34f5888a005b1729061
Jessie Grosen <jmgrosen@gmail.com>
2025-01-12 22:08:32 -0500

    very basic graph layout working with elk and reactflow

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,92 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import { ReactFlow } from '@xyflow/react';
+import { useSWR } from 'swr';
+import ELK from 'elkjs/lib/elk-api';
+ 
+import './App.css';
+import '@xyflow/react/dist/style.css';
+
+const elk = new ELK({
+  workerFactory: () =>
+    new Worker(new URL('elkjs/lib/elk-worker.min.js', import.meta.url)),
+});
+
+const initialNodes = {
+  '1': { data: { label: '1' } },
+  '2': { data: { label: '2' } }
+};
+const initialEdges = {
+  'e1-2': { source: '1', target: '2' }
+};
+
+const fetcher = (...args) => fetch(...args).then(res => res.json());
+
+function Flow({graph}) {
+  type GraphLayoutState =
+    {state: "laying"} |
+    {state: "laid", nodes: any, edges: any} |
+    {state: "error"};
+  const [state, setState] = useState<GraphLayoutState>({state: "laying"});
+  console.log(graph);
+
+  useEffect(() => {
+    const elkGraph = {
+      id: "root",
+      layoutOptions: {
+        'elk.algorithm': 'layered',
+        'elk.direction': 'DOWN',
+        'elk.spacing.nodeNode': 200,
+        'elk.spacing.edgeNode': 200,
+        'elk.layered.spacing.nodeNodeBetweenLayers': 200
+      },
+      children: Object.keys(graph.nodes).map(id => ({
+        id
+      })),
+      edges: Object.entries(graph.edges).map(([id, e]) => ({
+        id,
+        sources: [e.source],
+        targets: [e.target]
+      }))
+    };
+    elk.layout(elkGraph)
+       .then(layout => {
+         setState({
+           state: "laid",
+           nodes: layout.children.map(n => ({
+             id: n.id,
+             position: {
+               x: n.x,
+               y: n.y
+             },
+             ...graph.nodes[n.id]
+           })),
+           edges: layout.edges.map(e => ({
+             id: e.id,
+             source: e.sources[0],
+             target: e.targets[0],
+             ...graph.edges[e.id]
+           }))
+         });
+       })
+      .catch(e => {
+        console.log(e);
+        setState({state: "error"});
+      });
+  }, [graph]);
+
+  if (state.state === "laying") {
+    return <p>laying out the graph...</p>;
+  } else if (state.state == "error") {
+    return <p>error laying it out??</p>;
+  } else {
+    return <ReactFlow nodes={state.nodes} edges={state.edges} />;
+  }
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -28,8 +110,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div style={{ width: '600px', height: '400px'}}>
+        <Flow graph={{nodes: initialNodes, edges: initialEdges}} />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
