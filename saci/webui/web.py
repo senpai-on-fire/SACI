46687014169dc3d6d83468d1a9daf065d1839aeb
Jessie Grosen <jmgrosen@gmail.com>
2025-02-13 12:18:09 -0500

    web: rip out old code

--- saci/webui/web.py
+++ saci/webui/web.py
@@ -19,7 +19,6 @@ import httpx
 from fastapi import FastAPI, HTTPException, Query, Request, WebSocket, WebSocketDisconnect
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
-from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel, Field
 from websockets.asyncio.client import connect as ws_connect, ClientConnection as WsClientConnection
 from websockets.exceptions import InvalidStatus as WsInvalidStatus, ConnectionClosedOK as WsConnectionClosedOK
@@ -37,63 +36,19 @@ from ..modeling.device import Wifi, Motor, GPSReceiver
 
 l = logging.getLogger(__name__)
 
-app = FastAPI()
-app.mount("/static", StaticFiles(directory="saci/webui/static"), name="static")
-app.mount("/assets", StaticFiles(directory="web/dist/assets"), name="assets")
-templates = Jinja2Templates(directory="saci/webui/templates")
-
 start_work_thread()
 
-APP_CONTROLLER_URL = os.environ.get("APP_CONTROLLER_URL", "http://localhost:3000")
+app = FastAPI()
 
+### Endpoints for the frontend UI
 
-def kill_all_apps():
-    apps_resp = httpx.get(f"{APP_CONTROLLER_URL}/api/app")
-    for app_json in apps_resp.json():
-        app_id = app_json['id']
-        l.info(f"killing app {app_id}")
-        httpx.post(f"{APP_CONTROLLER_URL}/api/app/{app_id}/stop")
-        httpx.delete(f"{APP_CONTROLLER_URL}/api/app/{app_id}")
-
-# kill all existing apps when we start. we should probably not have this behavior permanently
-try:
-    kill_all_apps()
-except httpx.ConnectError:
-    l.warning("can't connect to app-controller, is it up and the URL configured correctly?")
-
-def get_component_abstractions(comp) -> dict[str, str]:
-    if hasattr(comp, "ABSTRACTIONS"):
-        d = {"99": "-"} | dict((str(level), abs_obj.__class__.__name__) for level, abs_obj in comp.ABSTRACTIONS.items())
-        return d
-    return {}
-
-
-# my dumb JSON serializer
-def json_serialize(obj) -> int | str | bool | list | dict:
-    if isinstance(obj, dict):
-        return dict((k, json_serialize(v)) for k, v in obj.items())
-    elif isinstance(obj, (tuple, list)):
-        return [json_serialize(item) for item in obj]
-    elif isinstance(obj, (int, str, bool)):
-        return obj
-    elif obj is None:
-        return "None"
-    else:
-        # object
-        if hasattr(obj, "to_json_dict") and callable(obj.to_json_dict):
-            return json_serialize(obj.to_json_dict())
-        elif hasattr(obj, "__slot__"):
-            d = {}
-            for attr in obj.__slot__:
-                d[attr] = json_serialize(getattr(obj, attr))
-            return d
-        else:
-            return repr(obj)
-
+app.mount("/assets", StaticFiles(directory="web/dist/assets"), name="assets")
 
 @app.get("/")
 async def serve_frontend_root():
     return FileResponse("web/dist/index.html")
+
+### Endpoints for blueprint management
 
 class ComponentModel(BaseModel):
     name: str
@@ -110,8 +65,6 @@ ComponentID = str
 def comp_id(comp: ComponentBase) -> ComponentID:
     # the graph is based on object identity, so i don't really see a better option here
     return str(id(comp))
-
-AnalysisID = str
 
 class HypothesisModel(BaseModel):
     name: str
@@ -142,6 +95,47 @@ def get_blueprints() -> dict[BlueprintID, DeviceModel]:
     # TODO: store the hypotheses per-blueprint
     return {bp_id: blueprint_to_model(bp, hypotheses[bp_id]) for bp_id, bp in blueprints.items()}
 
+@app.post("/api/ingest_blueprint")
+def ingest_blueprint(name: str, serialized: dict, force: bool = False):
+    if name in blueprints:
+        return {"error": "exists"}, 400
+    try:
+        # TODO: move this to another thread and return a promise?
+        ingest(serialized, INGESTION_DIR / name, force=force)
+    except FileExistsError as e:
+        return {"error": "exists"}, 400
+    except ValueError as e:
+        print(e)
+        return {"error": "couldn't deserialize blueprint"}, 400
+
+    importlib.invalidate_caches()
+    # TODO: probably don't need to reload *all* the ingested modules
+    importlib.reload(ingested)
+    # TODO: should probably just separate builtin and ingested blueprints...
+    blueprint_id = "ingested/" + name
+    device = ingested.devices[blueprint_id]
+    blueprints[blueprint_id] = device
+
+    return {"id": blueprint_id, "name": device.name}
+
+### Endpoints for launching analyses using app-controller
+
+APP_CONTROLLER_URL = os.environ.get("APP_CONTROLLER_URL", "http://localhost:3000")
+
+def kill_all_apps():
+    apps_resp = httpx.get(f"{APP_CONTROLLER_URL}/api/app")
+    for app_json in apps_resp.json():
+        app_id = app_json['id']
+        l.info(f"killing app {app_id}")
+        httpx.post(f"{APP_CONTROLLER_URL}/api/app/{app_id}/stop")
+        httpx.delete(f"{APP_CONTROLLER_URL}/api/app/{app_id}")
+
+# kill all existing apps when we start. we should probably not have this behavior permanently
+try:
+    kill_all_apps()
+except httpx.ConnectError:
+    l.warning("can't connect to app-controller, is it up and the URL configured correctly?")
+
 class AnalysisUserInfo(BaseModel):
     """User-level metadata associated with an analysis type the user can run."""
     name: str
@@ -165,6 +159,8 @@ class Analysis:
             "images": self.images,
             "always_pull_images": False,
         }
+
+AnalysisID = str
 
 @app.get("/api/blueprints/{bp_id}/analyses")
 def get_analyses(bp_id: str) -> dict[AnalysisID, AnalysisUserInfo]:
@@ -233,172 +229,6 @@ async def vnc_proxy(*, websocket: WebSocket, app_id: int):
         _, rest = e.split(lambda e: isinstance(e, WebSocketDisconnect) and e.code in (1000, 1001, 1005))
         if rest is not None:
             raise rest
-
-@app.get("/api/cpv_info")
-def cpv_info(name: str):
-    for cpv in CPVS:
-        if cpv.__class__.__name__ == name:
-            return {
-                "name": cpv.NAME,
-                "entry_component": cpv.entry_component.__class__.__name__ if cpv.entry_component else "N/A",
-                "exit_component": cpv.exit_component.__class__.__name__ if cpv.exit_component else "N/A",
-                "required_components": [comp.__class__.__name__ for comp in cpv.required_components],
-                "initial_conditions": cpv.initial_conditions or {},  # Include initial_conditions
-                "vulnerabilities": [vuln.__class__.__name__ for vuln in cpv.vulnerabilities],  # Extract vulnerabilities
-                "reference_urls": cpv.reference_urls,  # Include reference_urls directly
-                "attack_requirements": cpv.attack_requirements,
-                "attack_vectors": [
-                    {
-                        "name": vector.name,
-                        "signal": vector.signal.modality,
-                        "access_level": vector.required_access_level,
-                        "configuration": vector.configuration,  # Pass configuration as a dictionary
-                    }
-                    for vector in cpv.attack_vectors
-                ],
-                "impact": [
-                    {"category": impact.category, "description": impact.description}
-                    for impact in cpv.attack_impacts
-                ],
-                "exploit_steps": cpv.exploit_steps,
-            }
-    return {"error": "CPV not found"}, 400
-
-
-def lookup_blueprint(raw):
-    try:
-        blueprint_id = int(raw)
-        if blueprint_id >= len(blueprints):
-            return None
-        return blueprints[blueprint_id]
-    except (TypeError, ValueError):
-        return None
-
-@app.post("/api/ingest_blueprint")
-def ingest_blueprint(name: str, serialized: dict, force: bool = False):
-    if name in blueprints:
-        return {"error": "exists"}, 400
-    try:
-        # TODO: move this to another thread and return a promise?
-        ingest(serialized, INGESTION_DIR / name, force=force)
-    except FileExistsError as e:
-        return {"error": "exists"}, 400
-    except ValueError as e:
-        print(e)
-        return {"error": "couldn't deserialize blueprint"}, 400
-
-    importlib.invalidate_caches()
-    # TODO: probably don't need to reload *all* the ingested modules
-    importlib.reload(ingested)
-    # TODO: should probably just separate builtin and ingested blueprints...
-    blueprint_id = "ingested/" + name
-    device = ingested.devices[blueprint_id]
-    blueprints[blueprint_id] = device
-
-    return {"id": blueprint_id, "name": device.name}
-
-@app.get("/api/get_blueprint")
-def get_blueprint(blueprint_id: Annotated[str, Query(alias="id")]):
-    if blueprint_id not in blueprints:
-        return {"error": "Blueprint not found"}
-    
-    cps = blueprints[blueprint_id]
-    
-    d = {
-        "nodes": [],
-        "links": [],
-        "options": {},
-    }
-    
-    for node in cps.component_graph:
-        d["nodes"].append({
-        "id": id(node), 
-        "name": repr(node),
-        "entry": cps.component_graph.nodes[node].get('is_entry', False)
-        })
-    for src, dst in cps.component_graph.edges:
-        d["links"].append({
-            "source": id(src),
-            "target": id(dst),
-        })
-    
-    for option in cps.options:
-        d["options"][option] = cps.get_option(option)
-    
-    return {
-        "component_graph": d
-    }
-
-
-@app.post("/api/set_blueprint_option")
-def set_blueprint_option(
-    blueprint_id: Annotated[str, Query(alias="id")],
-    option: str,
-    value: dict,
-):
-    if blueprint_id not in blueprints:
-        return {"error": "Blueprint not found"}
-    cps = blueprints[blueprint_id]
-
-    cps.set_option(option, value)
-    #print(cps.steering.has_aps)
-
-    return {}
-
-@app.get("/api/cpv_search")
-def cpv_search(blueprint_id: str):
-    if blueprint_id not in blueprints:
-        return {"error": "Blueprint not found"}
-
-    cps = blueprints[blueprint_id]
-    search_id = add_search(cps=cps)
-
-    # Wait briefly for the worker to start
-    time.sleep(1)
-
-    search = SEARCHES.get(search_id, {})
-    if not search.get("cpv_inputs"):
-        return {"error": "No CPVs found for this CPS."}
-
-    # Return only ID and name of each CPV
-    return {"cpvs": search["cpv_inputs"]}
-
-@app.get("/api/cpv_search_ids")
-def cpv_search_ids():
-
-    ids = list(SEARCHES)
-
-    return {
-        "ids": ids,
-    }
-
-@app.get("/api/cpv_search_result")
-def cpv_search_result(search_id: Annotated[str, Query(alias="id")]):
-    if search_id is None:
-        return {
-            "error": "Search ID cannot be None",
-        }
-
-    try:
-        search = SEARCHES.get(int(search_id), None)
-    except TypeError:
-        search = None
-
-    if search is None:
-        return {
-            "error": "Search not found"
-        }
-
-    result = {
-        "taken": search.get("taken", False),
-        "result": search.get("result", None),
-        # "identified_cpv_and_paths": search.get("identified_cpv_and_paths", None),
-        "cpv_inputs": search.get("cpv_inputs", None),
-        "last_updated": search.get("last_updated", int(time.time() * 10000)),
-        "tasks": search.get("tasks", None),
-    }
-
-    return json_serialize(result)
 
 # delayed import
 from saci_db.devices import devices, ingested
