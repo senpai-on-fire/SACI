cf4928a1febeb9075b8c4209447b76cd2e7b44c0
Daniel-xsy <1491387884@qq.com>
2025-03-03 11:47:55 -0800

    add BaseCompEffect for descripe component-level attack impact

--- saci/modeling/attack/base_attack_impact.py
+++ saci/modeling/attack/base_attack_impact.py
@@ -1,6 +1,13 @@
 
 class BaseAttackImpact:
+    """Attack impact for CPS"""
     def __init__(self, category=None, description=None):
         self.category = category
         self.description = description
 
+
+class BaseCompEffect:
+    """Attack impact for component"""
+    def __init__(self, category=None, description=None):
+        self.category = category
+        self.description = description
