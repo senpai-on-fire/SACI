d5adf362b64128f1f4364b3afd7f5646a4c912cd
Kevin Phoenix <kevin@kphoenix.us>
2025-08-25 13:24:09 -0700

    Include tests in ruff CI

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -17,9 +17,9 @@ from saci.modeling.device.interface.serial import Serial
 from saci.modeling.device.gcs import GCS
 from saci.modeling.device import Device
 from saci.modeling.state import GlobalState
-from saci.modeling.device import ComponentID, MultiCopterMotor, Wifi, SikRadio
+from saci.modeling.device import ComponentID, MultiCopterMotor, Wifi
 from saci.orchestrator import process, identify
-from saci.hypothesis import Hypothesis, ParameterAssumption, RemoveComponentsAssumption, AddComponentAssumption
+from saci.hypothesis import Hypothesis, ParameterAssumption, RemoveComponentsAssumption
 from saci.identifier import IdentifierCPV
 
 from saci_db.cpvs import MavlinkSiKCPV, SerialRollOverCPV, CompassPermanentSpoofingCPV, WifiWebCrashCPV
@@ -41,7 +41,7 @@ class TestPipeline(unittest.TestCase):
     def test_end_to_end(self):
         cps, database, initial_state = generate_fake_data()
         try:
-            all_cpvs = process(cps, database, initial_state)
+            process(cps, database, initial_state)
         except TypeError:
             # TODO: implement a real constrainer to avoid this exception
             pass
@@ -54,9 +54,12 @@ class TestPipeline(unittest.TestCase):
     def test_identifier_mavlink_cpv(self):
         cps, _, initial_state = generate_fake_data()
         _, cpv_paths = identify(cps, initial_state, cpv_model=MavlinkSiKCPV())
-        path = cpv_paths[0].path
-        self.assertIsInstance(path[0].component, GCS)
-        self.assertIsInstance(path[-1].component, MultiCopterMotor)
+        self.assertIsNotNone(cpv_paths)
+        if cpv_paths is not None:
+            self.assertGreater(len(cpv_paths), 0)
+            path = cpv_paths[0].path
+            self.assertIsInstance(path[0].component, GCS)
+            self.assertIsInstance(path[-1].component, MultiCopterMotor)
 
     def test_identifier_rover(self):
         cps = NGCRover()
@@ -74,12 +77,13 @@ class TestPipeline(unittest.TestCase):
             if endpoints is not None:
                 start, end = endpoints
                 self.assertIsNotNone(cpv_paths)
-                self.assertGreater(len(cpv_paths), 0)
-                for cpv_path in cpv_paths:
-                    self.assertIsInstance(cpv_path.path[0].component, start)
-                    self.assertIsInstance(cpv_path.path[-1].component, end)
+                if cpv_paths is not None:
+                    self.assertGreater(len(cpv_paths), 0)
+                    for cpv_path in cpv_paths:
+                        self.assertIsInstance(cpv_path.path[0].component, start)
+                        self.assertIsInstance(cpv_path.path[-1].component, end)
             else:
-                self.assertIsNone(cpv_paths)
+                self.assertTrue(cpv_paths is None or len(cpv_paths) == 0)
 
     def test_gps_spoofying_hypothesis(self):
         cps = NGCRover()
@@ -102,12 +106,12 @@ class TestPipeline(unittest.TestCase):
                     "motor",
                 }
             },
-            "Looks like we added or removed some components from the NGCRover device model. Fix this test appropriately.",
+            "NGCRover components changed.",
         )
         self.assertNotIn(
             "signal_strength_threshold",
             cps.components[ComponentID("gps")].parameters,
-            "Oops, looks like we added a GPS signal strength threshold to the NGCRover device model. Fix this test to use some other unset parameter.",
+            "Unexpected GPS signal strength threshold parameter.",
         )
 
         threshold = -70
@@ -131,17 +135,17 @@ class TestPipeline(unittest.TestCase):
         self.assertNotIn(
             "signal_strength_threshold",
             cps.components[ComponentID("gps")].parameters,
-            "The device didn't get copied correctly and so transforming the copy also transformed the original. Womp womp.",
+            "Device copy failed; original was mutated.",
         )
         self.assertIn(
             "signal_strength_threshold",
             transformed_cps.components[ComponentID("gps")].parameters,
-            "Hm, the hypothesis didn't set the parameter in the rewritten CPS at all.",
+            "Parameter not set in transformed CPS.",
         )
         self.assertEqual(
             transformed_cps.components[ComponentID("gps")].parameters["signal_strength_threshold"],
             threshold,
-            "... the hypothesis transformation set the parameter to *something* but not the specified value? How did that manage to happen?",
+            "Parameter set incorrectly in transformed CPS.",
         )
         self.assertEqual(
             set(transformed_cps.components),
@@ -160,7 +164,7 @@ class TestPipeline(unittest.TestCase):
                     "motor",
                 }
             },
-            "Looks like we added or removed some components from the NGCRover device model. Fix this test appropriately.",
+            "Transformed NGCRover components changed.",
         )
 
     def test_compvuln_effects(self):
@@ -173,17 +177,16 @@ class TestPipeline(unittest.TestCase):
         copied_cps = deepcopy(cps)
         self.assertFalse(
             copied_cps.component_graph.nodes[ComponentID("wifi")]["is_entry"],
-            "Did changed component graph attributes not get copied over?",
+            "Component graph attributes not copied.",
         )
         self.assertTrue(
             wifi_auth_vuln.exists(copied_cps),
-            "Wifi auth vuln not found on the NGCRover.",
+            "Wifi auth vuln not found.",
         )
         wifi_auth_vuln.apply_effects(copied_cps)
-        # The open wifi authentication should allow the wifi component to be an entry point now.
         self.assertTrue(
             copied_cps.component_graph.nodes[ComponentID("wifi")]["is_entry"],
-            "Wifi auth vuln did not apply the expected effect despite indicating existence.",
+            "Wifi auth vuln effect not applied.",
         )
 
         # Make sure the CPV-based identifier is applying component vulnerabilities' effects
@@ -193,17 +196,18 @@ class TestPipeline(unittest.TestCase):
         self.assertIn(
             cpv_paths_without_vulns,
             (None, []),
-            "Shouldn't be able to find a wifi-based CPV when the wifi module isn't an entry point",
+            "Shouldn't find wifi-based CPV without entry point.",
         )
         vulns = [wifi_auth_vuln]
         _, cpv_paths_with_vulns = identify(cps, initial_state, cpv_model=cpv, vulns=vulns)
         self.assertIsNotNone(
-            cpv_paths_with_vulns, "Should find a wifi-based CPV when the wifi auth vuln adds wifi as an entry point"
+            cpv_paths_with_vulns,
+            "Should find wifi-based CPV with vuln.",
         )
         self.assertGreater(
             len(cpv_paths_with_vulns),  # type: ignore
             0,
-            "Should find a wifi-based CPV when the wifi auth vuln adds wifi as an entry point",
+            "Should find wifi-based CPV with vuln.",
         )
 
     def test_identifier_hypothesis(self):
@@ -215,13 +219,13 @@ class TestPipeline(unittest.TestCase):
         wifi_auth_vuln = LackWifiAuthenticationVuln()
         self.assertTrue(
             wifi_auth_vuln.exists(cps),
-            "Wifi auth vuln not found on the NGCRover.",
+            "Wifi auth vuln not found.",
         )
         effects = wifi_auth_vuln.effects(cps)
         self.assertEqual(
             len(effects),
             1,
-            "Oops, fix this test to take into account that LackWifiAuthenticationVuln returns more (or fewer?) than one effect now.",
+            "Unexpected number of effects.",
         )
         effect = effects[0]
 
@@ -245,7 +249,7 @@ class TestPipeline(unittest.TestCase):
         )
         self.assertFalse(
             identifier.check_hypothesis(cpv, hypothesis_no_annotations),
-            "Without annotations, the hypothesis shouldn't find a match, but it did!",
+            "Should not match without annotations.",
         )
 
         # Verify that with an annotation added that has the effects of LackWifiAuthenticationVuln, the WifiWebCrashCPV
@@ -264,7 +268,7 @@ class TestPipeline(unittest.TestCase):
         )
         self.assertTrue(
             identifier.check_hypothesis(cpv, hypothesis_with_annotations),
-            "With annotations, the hypothesis should find a match, but it didn't!",
+            "Should match with annotations.",
         )
 
     def test_device_int_compids(self):
